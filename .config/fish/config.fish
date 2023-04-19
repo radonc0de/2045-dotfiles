1b33f9d2458f5dadcb27e28759d371c50a0e44f2
Keith L <keithlehman@proton.me>
2023-04-19 02:14:23 -0400

    added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vim=nvim
+alias pvpn=protonvpn-cli
+alias sl='echo "Bro forgot how to spell LMAOOO! the fuck is sl???? Stupid ass mf"; ls'
+alias eq=pulsemixer
+alias rdp='xfreerdp /u:tux /v:172.16.1.157 /p:pass /size:2246x1462 -grab-keyboard'
