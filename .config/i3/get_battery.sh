93feaa404321c4b1bd6edb9f22d8d0986fbb97b0
Keith L <keithlehman@proton.me>
2023-04-20 16:22:40 -0400

    fixed battery script, added to fish config

--- .config/i3/get_battery.sh
+++ .config/i3/get_battery.sh
@@ -4,14 +4,14 @@
 acpi=$(acpi -i | grep -E '[0-9][0-9]:[0-9][0-9]:[0-9][0-9]')
 time=$(acpi -i | grep -o -E '[0-9][0-9]:[0-9][0-9]:[0-9][0-9]')
 status=$(echo "$acpi" | awk '{print $3}')
-capacity=$(echo "$acpi" | awk '{print $4}')
+capacity=$(acpi -i | grep -o -E '[0-9]*%' | head -n 1 | awk '{sub("%", ""); print $0}')
 #status=$(cat /sys/class/power_supply/BAT1/status)
 
-if [ $status == "Charging" ]; then
+if [ $status = "Charging" ]; then
     echo 🔋⚡ $capacity $time
-elif [ $capacity -le 15 ]; then 
+elif [ "$capacity" -le 15 ]; then 
     echo 🪫⚠️ $capacity $time
-elif [ $capacity -le 30 ]; then 
+elif [ "$capacity" -le 30 ]; then 
     echo 🪫 $capacity $time
 else 
     echo 🔋 $capacity $time
